a4b785fbc5858bb193458e7fef4b3ac6fab735c8
Christina <ga95yaw@mytum.de>
2015-10-23 15:06:12 +0200

    add HelloWorld class

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-
+import java.util.Scanner;
 /**
  * @author Tobias
  *
@@ -13,6 +13,18 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Please enter your name: ");
+		
+	    String username = scanner.next();
+	    
+	    HelloUser helloUser = new HelloUser(username);
+		helloUser.greetUser();
 	}
+	
 
 }
+
+
